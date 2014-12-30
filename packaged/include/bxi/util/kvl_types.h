5285194dc3380fad9d3102a4acf9a7687a204361
Alain Cady <alain.cady@atos.net>
2014-12-30 13:53:03 +0000

    Leaner packaging

---
+++ packaged/include/bxi/util/kvl_types.h
@@ -0,0 +1,4 @@
+    /* Types used in the lexer: can't be modified! */
+    long *num;
+    char *str;
+    bxivector_p tuple;
--- tests/test_kvl.c
+++ tests/test_kvl.c
@@ -28,10 +28,7 @@
 /* KeVaLa */
 union YYSTYPE
 {
-    /* Types used in the lexer: can't be modified */
-    long *num;
-    char *str;
-    bxivector_p tuple;
+    #include <bxi/util/kvl_types.h>
 };
 #include <bxi/util/kvl.h>
 
