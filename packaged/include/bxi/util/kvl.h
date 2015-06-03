3ae21abaebbc4566413f434ca7fde55b9c1a8c86
Alain Cady <alain.cady@atos.net>
2015-06-03 15:29:03 +0000

    kvl documentation first draft

--- packaged/include/bxi/util/kvl.h
+++ packaged/include/bxi/util/kvl.h
@@ -1,4 +1,3 @@
-
 /* -*- coding: utf-8 -*-
  ###############################################################################
  # Author: Alain Cady <alain.cady@bull.net>
@@ -27,7 +26,7 @@
 
 /**
  * @file    kvl.h
- * @brief   Kev/Value lexer.
+ * @brief   a Key/Value lexer.
  *
  * TODO: document.
  */
@@ -41,6 +40,11 @@
 
 #ifndef YY_TYPEDEF_YY_EXTRA_T
     #define YY_TYPEDEF_YY_EXTRA_T
+        /**
+         * Custom structure to store extra information such as filename of the current fd.
+         *
+         * It also holds helpers such as pointers to internal buffer / functions used while lexing.
+         */
         typedef struct yyextra_data_t_s {
             char *filename;
             char *buf;
@@ -75,6 +79,11 @@ typedef union YYSTYPE YYSTYPE;
 #if ! defined YYLTYPE && ! defined YYLTYPE_IS_DECLARED
 # define YYLTYPE_IS_DECLARED 1
 # define YYLTYPE_IS_TRIVIAL 1
+/**
+ * Structure to keep track of tokens' textual locations.
+ *
+ * yylex fills it while tokenizing, to be used by the parser, e.g. on error reporting.
+ */
 typedef struct YYLTYPE
 {
   int first_line;
@@ -88,14 +97,56 @@ typedef struct YYLTYPE
 #define YY_EXTRA_TYPE yyextra_data_s*
 /* End of Bison/Flex binding */
 
-/* Flex utils */
-enum yytokentype yylex(YYSTYPE *, YYLTYPE*, yyscan_t);
+/**
+ * Flex lexing function
+ *
+ * @param[out] yylval_param lexed value
+ * @param[out] yylloc_param token' textual locations
+ * @param[in] yyscanner the scanner to extract next token from
+ *
+ * @return Token ID
+ * @see yytokentype
+ */
+enum yytokentype yylex(YYSTYPE *yylval_param, YYLTYPE *yylloc_param, yyscan_t yyscanner);
 #define YY_DECL enum yytokentype yylex \
                 (YYSTYPE *yylval_param, YYLTYPE *yylloc_param, yyscan_t yyscanner)
 
+/**
+ * Scanner initialization from file name
+ *
+ * @param[in] fname file path of the file to tokenize
+ * @param[in] kw_lookup_fnt fuction to define your own token id
+ *
+ * @return a scanner object to pass to yylex function
+ */
 yyscan_t kvl_init(char *fname, enum yytokentype (*kw_lookup_fnt)(char*));
+
+/**
+ * Scanner initialization from file descriptor
+ *
+ * @param[in] file_in file descriptor to tokenize
+ * @param[in] fname optional filename, used for error message generation
+ * @param[in] kw_lookup_fnt fuction to define your own token id
+ *
+ * @return a scanner object to pass to yylex function
+ */
 yyscan_t kvl_init_from_fd(FILE *file_in, char *fname, enum yytokentype (*kw_lookup_fnt)(char*));
+
+/**
+ * Scanner proper clean-up
+ *
+ * @param[in] scanner The scanner to clean-up
+ */
 void kvl_finalize(yyscan_t scanner);
+
+/**
+ * Return the extra structure associated with the given scanner.
+ *
+ * @param[in] scanner The considered scanner
+ *
+ * @return The structure containing scanner specific extra data
+ * @see yyextra_data_t_s
+ */
 YY_EXTRA_TYPE yyget_extra(yyscan_t scanner);
 
 #define KVL_LOG_NAME "kvl"
