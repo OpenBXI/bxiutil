ad87f26a1f99c6186ad9a41a9fe051e9721593cc
Pierre Imbaud <pierre.imbaud@ext.bull.net>
2016-08-03 09:01:19 +0200

    interface now works on scs5; better error, better doc, pep8

--- packaged/lib/bxi/util/interface.py
+++ packaged/lib/bxi/util/interface.py
@@ -13,30 +13,39 @@
 """ determine interface to reach a given target
 
     use the ip route command to determine which interface, among
-    current host interfaces, is used reach a given target
+    current host interfaces, is used to reach a given target
+
+    The original purpose is to know which "trapDestination" one
+    should provide to a bxi divio.
 """
 
 import subprocess as sp
 import socket
 import re
 
+import bxi.base.err as bxierr
+
+
 def interface(target):
-    
+
     """ use the ip route command to determine which interface, among
-        current host interfaces, is used reach a given target
+        current host interfaces, is used to reach a given target
 
         @param target: host to reach, ip or dns name
         @return interface ip address
     """
     ip = socket.gethostbyname(target)
-    command = ("ip route get %s" %ip).split()
+    command_string = ("ip route get %s" % ip)
+    command = command_string.split()
     out = sp.check_output(command)
     lines = out.split('\n')
     l0 = lines[0]
-    reg = re.compile('(^[0-9.]+) via ([0-9.]+).*src ([0-9.]+)')
+    reg = re.compile('.*src ([0-9.]+)')
     match = reg.match(l0)
+    if not match:
+        template = "command '%s'\nproduced '%s'\nthat does not match regex '%s'"
+        msg = template % (command_string, l0, reg.pattern)
+        raise bxierr.BXIError(msg)
     assert match
     interface = match.groups()[-1]
-    print match.groups()
     return interface
-    
