c4298f212b686ebb0d5f71e3b4292a40b0605faa
Jean-Noel Quintin <jean-noel.quintin@bull.net>
2017-06-13 14:26:03 +0200

    Test travis with -i for sudo

--- misc/travis/install-dependencies.sh
+++ misc/travis/install-dependencies.sh
@@ -14,14 +14,13 @@ wget https://github.com/zeromq/libzmq/releases/download/v4.2.1/zeromq-4.2.1.tar.
     cd ${TMPDIR}/backtrace-1.2.0 && \
     ./configure --prefix=/usr/ && \
     make  && \
-    sudo make -j${CPUS_NB} install && \
+    sudo -i make -j${CPUS_NB} install && \
     wget https://github.com/OpenBXI/bxibase/archive/7.0.0-Bull.1.0.tar.gz && \
     tar xvf 7.0.0-Bull.1.0.tar.gz -C ${TMPDIR} && \
     cd ${TMPDIR}/bxibase-7.0.0-Bull.1.0 && \
     ./bootstrap.sh &&\
     ./configure  --prefix=/usr/ && \
     make -j${CPUS_NB} && \
-    sudo make -j${CPUS_NB} install && \
-    sudo ldconfig && \
-    python -c 'import bxi.base'
+    sudo -i make -j${CPUS_NB} install && \
+    sudo ldconfig
 
