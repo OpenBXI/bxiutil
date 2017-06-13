bd86fd7bbf64e0cd12f79afd3c8030c95f67c25b
Jean-Noel Quintin <jean-noel.quintin@bull.net>
2017-06-13 09:13:22 +0200

    Correct travis installation

--- misc/travis/install-dependencies.sh
+++ misc/travis/install-dependencies.sh
@@ -17,7 +17,7 @@ wget https://github.com/zeromq/libzmq/releases/download/v4.2.1/zeromq-4.2.1.tar.
     sudo make -j${CPUS_NB} install && \
     wget https://github.com/OpenBXI/bxibase/archive/7.0.0-Bull.1.0.tar.gz && \
     tar xvf backtrace-1.2.0.tar.gz -C ${TMPDIR} && \
-    cd ${TMPDIR}/backtrace-1.2.0 && \
+    cd ${TMPDIR}/bxibase-7.0.0-Bull.1.0 && \
     ./configure  && \
     make -j${CPUS_NB} && \
     sudo make -j${CPUS_NB} install && \
