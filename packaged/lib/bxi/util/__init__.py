a119fda79279bd103e3a7eb5de71aa55a4287d75
John Gliksberg <john.gliksberg@atos.net>
2017-09-18 15:36:39 +0200

    [minor] Correct description of bxi.util

--- packaged/lib/bxi/util/__init__.py
+++ packaged/lib/bxi/util/__init__.py
@@ -2,11 +2,11 @@
 
 """
 @authors Pierre Vignéras <pierre.vigneras@bull.net>
-@copyright 2013  Bull S.A.S.  -  All rights reserved.\n
+@copyright 2015  Bull S.A.S.  -  All rights reserved.\n
            This is not Free or Open Source software.\n
            Please contact Bull SAS for details about its license.\n
            Bull - Rue Jean Jaurès - B.P. 68 - 78340 Les Clayes-sous-Bois
-@namespace bxi.base Python BXI Base module
+@namespace bxi.util Python BXI Utilitaries
 
 """
 from __future__ import print_function
